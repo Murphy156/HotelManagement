d13f0ffbda4f8dc69c1b68613f1515992b0cdc57
lishuohua <lishuohua@37.com>
2020-08-23 16:26:24 +0800

    git tracks changes

--- core/templates/static/js/tenantManagement.js
+++ core/templates/static/js/tenantManagement.js
@@ -209,3 +209,41 @@ deleteUser = function(obj){
         });
     }
 }
+
+function UpladFile(fileObj) {
+                var form = new FormData(); // FormData 对象
+                form.append("file", fileObj); // 文件对象
+                url = "/api/v1/tenant/importUser"
+                $.ajax({
+                    url: url,                      //url地址
+                    type: 'POST',                 //上传方式
+                    data: form,                   // 上传formdata封装的数据
+                    dataType: 'JSON',
+                    cache: false,                  // 不缓存
+                    processData: false,        // jQuery不要去处理发送的数据
+                    contentType: false,         // jQuery不要去设置Content-Type请求头
+                    success:function (data) {           //成功回调
+                        console.log(data);
+                    },
+                   error:function (data) {           //失败回调
+                        console.log(data);
+                    }
+                });
+             }
+
+function upload() {
+        $("#file").click();
+        $('#file').change(function (e) {
+         var fileName = e.target.files[0];//js 获取文件对象
+         if(fileName !== undefined){
+           var file_typename =   fileName.name.substring(fileName.name.lastIndexOf('.'));
+           if (file_typename === '.xlsx' || file_typename === '.xls') {$("#filename").css("display","block");$("#filename").val(fileName.name);
+           UpladFile(fileName);
+           }else {
+            console.log("请选择正确的文件类型！")
+            }
+         }else{
+       console.log("请选择正确的文件！")
+        }
+});
+}
