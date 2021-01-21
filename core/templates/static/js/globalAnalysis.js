82bacfe0e1b8d2da2679213b8618421fb4bae271
lijunhua <634134551@qq.com>
2021-01-21 08:58:01 +0800

    new

--- core/templates/static/js/globalAnalysis.js
+++ core/templates/static/js/globalAnalysis.js
@@ -382,7 +382,7 @@ regipro = function(){
             myChart.setOption(option);
         }
         else {
-            alert("无数据")
+            alert("NO DATA")
         }
     });
 }
@@ -398,20 +398,42 @@ ratedeal = function(data){
 
 //出租率同比
 rateComp = function(){
+    var for_year = $('#global_year_choose option:selected').val()-1;
     var year = $('#global_year_choose option:selected').val();
-    var url = "/api/v1/globalanalysis/renRateCompar?year=" +year;
-    console.log(url);
+    var current_month = new Array();
+    var new_data = new Array();
+    var old_data = new Array();
+    var url1 = "/api/v1/globalanalysis/renRateCompar?year=" +year;
+    var url2 = "/api/v1/globalanalysis/renRateCompar?year=" +for_year;
+    console.log(url1);
+    console.log(url2);
 
-    $.get(url,function(data,status){
+    $.get(url1,function(data,status){
         if(status == 'success'){
             console.log(data)
-            var rentRate = ratedeal(data);
-            console.log(rentRate)
-            var x = new Array();
-            for (i = 0; i<rentRate.length; i++){
-                x[i] = i+1;
+            var new_data = ratedeal(data);
+            console.log(new_data)
+//            for (i = 0; i<new_data.length; i++){
+//                current_month[i] = i+1;
+//            }
+            console.log(current_month)
             }
-            console.log(x)
+        else {
+            alert("NO DATA")
+        }
+    });
+
+     $.get(url2,function(data,status){   //jquary中的get函数只能在其内部获取
+        if(status == 'success'){
+            console.log(data)
+            old_data = ratedeal(data);
+            for (i = 0; i<new_data.length; i++){
+                current_month[i] = i+1;
+            }
+            console.log(current_month)
+            console.log(old_data)
+            console.log(new_data)
+
             var myChart = echarts.init(document.getElementById('rateCompara'));
             var option = {
             backgroundColor: "#F0FFFF",
@@ -430,11 +452,11 @@ rateComp = function(){
                 }
             },
             legend: {
-                data:x
+                data:current_month
             },
             xAxis: {
                 type: "category",
-                data: x,
+                data: current_month,
                 axisLabel: {
                     show:true,
                     color:"rgba(86, 72, 72, 1)",
@@ -452,16 +474,28 @@ rateComp = function(){
             },
             series: [
             {
-                name: '出租率',
+                name: year,
+                color:'#003366',
                 type: 'line',
-                data: rentRate,
-                data: rentRate,
+                data: new_data,
                 label:{
                     show: true,
                     color:"rgba(86, 72, 72, 1)",
                     fontWeight:"bolder"
                 }
-            }]
+            },
+            {
+                name: for_year,
+                color:'#e5323e',
+                type: 'line',
+                data: old_data,
+                label:{
+                    show: true,
+                    color:"rgba(86, 72, 72, 1)",
+                    fontWeight:"bolder"
+                }
+            }
+            ]
         };
         myChart.setOption(option);
         }
